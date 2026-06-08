9577888856883f384358516fe9dec782fb2923b6
raheemsoomro890 <raheemsoomro282@gmail.com>
2026-06-08 09:39:29 +0500

    Switch to Nemotron free model

--- api/chat.js
+++ api/chat.js
@@ -15,7 +15,7 @@ module.exports = async function handler(req, res) {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "meta-llama/llama-3.1-8b-instruct:free",
+          model: "nvidia/nemotron-3-ultra-253b-v1:free",
           messages: [
             {
               role: "system",
